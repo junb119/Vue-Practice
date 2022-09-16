6fff796f5cd6314d3a0967cc58f6af9ba7546823
junb119 <emfowkd@gmail.com>
2022-09-16 17:07:41 +0900

    조건부 렌더링(v-show)

--- vue3-template-syntax/src/App.vue
+++ vue3-template-syntax/src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <button @click="handler">Click me!</button>
-  <template v-if="isShow">    <!-- <template>를 사용하면 console창에 불필요한 태그 표시 안됨 -->
-    <h1>Title</h1>
-    <p>Paragraph 1</p>
-    <p>Paragraph 2</p>
-  </template>
+  <h1 v-show="isShow">
+    <!-- v-if    VS    v-show  -->
+    <!-- v-show : 
+      - <template> 사용불가
+      - 항상 렌더링 되어 DOM(HTMl)에 남아있음. 
+      - 단순 display속성만 전환함(랜더링은 되어 console에 남음. 보이지만 않음)
+      - 초기비용 발생후 전환시 비용이 낮음 -> 자주 전환하면 v-show 아니면 v-if 추천      
+       -->
+    Hello?!
+  </h1>
 </template>
 
 <script>
