4d2badd470edf3d604c0e6869d40946f38660e9f
junb119 <emfowkd@gmail.com>
2022-09-16 17:56:53 +0900

    리스트 렌더링

--- vue3-template-syntax/src/App.vue
+++ vue3-template-syntax/src/App.vue
@@ -1,30 +1,53 @@
 <template>
   <button @click="handler">Click me!</button>
-  <h1 v-show="isShow">
-    <!-- v-if    VS    v-show  -->
-    <!-- v-show : 
-      - <template> 사용불가
-      - 항상 렌더링 되어 DOM(HTMl)에 남아있음. 
-      - 단순 display속성만 전환함(랜더링은 되어 console에 남음. 보이지만 않음)
-      - 초기비용 발생후 전환시 비용이 낮음 -> 자주 전환하면 v-show 아니면 v-if 추천      
-       -->
-    Hello?!
+  <h1 v-for="{id,name} in newfruits"
+  :key="id">
+    {{name}}-{{id}}
   </h1>
+
+  
+    <!-- 변이 메소드 
+    Vue는 감시중인 배열의 변이 메소드를 래핑하여 뷰 갱신을 트리거한다(=반응성)
+    push()
+    pop()
+    shift()
+    unshift()
+    splice() : index를 이용해 데이터를 넣거나 빼거나 삭제
+    sort()
+    reverse()
+  -->
+
+  <!-- 비변이 메소드(원래 배열을 변경 x) 
+    filter()
+    concat()
+    slice()
+  -->
 </template>
 
 <script>
+import shortid from 'shortid'
 export default {
   data () {
     return {
-      isShow : true,
-      count : 0
+      fruits : ['Apple','Banana','Cherry']
     }
   },
+
+  computed : {
+    newfruits () {
+
+      return this.fruits.map( fruit  => ({
+        id : shortid.generate(),
+        name : fruit
+      }))
+  }},
+
   methods: {
     handler () {
-      this.isShow = !this.isShow,
-      this.count += 1
+      this.fruits.push('orange')
     }
   }
+  
 }
 </script>
+
