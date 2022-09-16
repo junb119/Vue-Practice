8604a614cc508a5dcf4a0a9c163d537318172c29
junb119 <emfowkd@gmail.com>
2022-09-16 15:30:07 +0900

    watch

--- vue3-template-syntax/src/App.vue
+++ vue3-template-syntax/src/App.vue
@@ -1,14 +1,38 @@
 <template>
-  <Fruits />
+  <h1 @click="changeMessage">{{msg}}</h1>
+  <h1>{{reversedMessage}}</h1>
+
 </template>
 
 <script>
-import Fruits from './components/Fruits'
+// import Fruits from './components/Fruits'
 export default {
-  components : {
-    Fruits
-  }
-  
+  data () {
+    return {
+      msg : 'Hello?'
+    }
+  },
+  computed : {
+    reversedMessage() {
+      return this.msg.split('').reverse().join('')
+    }
+  },
+
+  watch : { // 데이터들의 변경사항을 감시하는 옵션
+    msg(newValue) {
+      console.log('msg :', newValue)
+    },
+
+    reversedMessage(newValue) {
+      console.log('reversedMessage :', newValue)
+    }
+  },
+
+  methods : {
+    changeMessage() {
+      this.msg = 'Good!'
+    }
+  }  
 }
 </script>
 
