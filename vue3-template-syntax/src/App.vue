ce3110cd0f9cee9a1cffe63fe11ea2526f4a46c9
junb119 <emfowkd@gmail.com>
2022-09-16 18:10:03 +0900

    이벤트 객체

--- vue3-template-syntax/src/App.vue
+++ vue3-template-syntax/src/App.vue
@@ -1,53 +1,21 @@
 <template>
-  <button @click="handler">Click me!</button>
-  <h1 v-for="{id,name} in newfruits"
-  :key="id">
-    {{name}}-{{id}}
-  </h1>
-
-  
-    <!-- 변이 메소드 
-    Vue는 감시중인 배열의 변이 메소드를 래핑하여 뷰 갱신을 트리거한다(=반응성)
-    push()
-    pop()
-    shift()
-    unshift()
-    splice() : index를 이용해 데이터를 넣거나 빼거나 삭제
-    sort()
-    reverse()
-  -->
-
-  <!-- 비변이 메소드(원래 배열을 변경 x) 
-    filter()
-    concat()
-    slice()
-  -->
+  <button @click="handler('hi', $event)"> {{msg}} </button>
+  <!-- 인수가 따로 있을 때 이벤트 객체를 받으려면 인수로 $event -->
 </template>
 
 <script>
-import shortid from 'shortid'
 export default {
   data () {
     return {
-      fruits : ['Apple','Banana','Cherry']
-    }
-  },
-
-  computed : {
-    newfruits () {
-
-      return this.fruits.map( fruit  => ({
-        id : shortid.generate(),
-        name : fruit
-      }))
+    msg : 'Click'
   }},
-
   methods: {
-    handler () {
-      this.fruits.push('orange')
+    handler (string, event) {
+      console.log(string),
+      console.log(event),
+      this.msg = 'Hello~!'
     }
   }
-  
 }
 </script>
 
