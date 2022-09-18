9c9d568fd1834c0fe09640392651e14b9f4b8559
junb119 <emfowkd@gmail.com>
2022-09-18 14:57:48 +0900

    폼 입력 바인딩(양방향 데이터 바인딩)

--- vue3-template-syntax/src/App.vue
+++ vue3-template-syntax/src/App.vue
@@ -1,19 +1,30 @@
 <template>
-  <input 
-    type="text"
-    @keydown="handler" />
-    <!-- keydown : 키를 눌렀을 때 이벤트 실행-->
+<!-- v-model : 양방향 데이터 바인딩 # 한글은 매끄럽게 적용안됨 -->
+  <h1>{{msg}}</h1>
+  <input type="text"
+  v-model="msg" />
+
+  <h1>{{checkbox}}</h1>
+  <input type="checkbox"
+  v-model="checkbox" />
+
+<!-- 한글 사용시 양방향 데이터 바인딩 (수동)-->
+  <h1>{{kor}}</h1>
+  <input type="text"
+  :value="kor"  
+  @input="kor = $event.target.value"/>
+
 </template>
+
 
 <script>
 export default {
-  methods : {
-    handler(event) {
-      if (event.key === 'Enter') {
-        console.log('Enter')
-      }
-    }
+  data () {
+    return {
+      msg : 'Hello world!',
+      checkbox : false,
+      kor : '안녕하세요.'
+    } 
   }
-  
 }
 </script>
