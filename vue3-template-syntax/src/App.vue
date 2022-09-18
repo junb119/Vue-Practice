4c014c5ade2cab611b911c4b399487ffba3b16d1
junb119 <emfowkd@gmail.com>
2022-09-18 15:13:05 +0900

    v-model 수식어[.lazy(=@change), .trim, .number]

--- vue3-template-syntax/src/App.vue
+++ vue3-template-syntax/src/App.vue
@@ -1,19 +1,24 @@
 <template>
-<!-- v-model : 양방향 데이터 바인딩 # 한글은 매끄럽게 적용안됨 -->
+<!-- v-model.lazy : 실시간으로 양방향 바인딩이 되는 것이 아닌 포커스가 해제될 때. -->
   <h1>{{msg}}</h1>
   <input type="text"
-  v-model="msg" />
+  v-model.lazy="msg" />
 
-  <h1>{{checkbox}}</h1>
-  <input type="checkbox"
-  v-model="checkbox" />
-
-<!-- 한글 사용시 양방향 데이터 바인딩 (수동)-->
+<!-- @change : v.model.lazy의 수동 버젼 -->
   <h1>{{kor}}</h1>
   <input type="text"
   :value="kor"  
-  @input="kor = $event.target.value"/>
+  @change="kor = $event.target.value"/>
 
+<!-- v-model.number : 데이터 바인딩 과정에서 문자로 형변환을 방지하여 숫자로 유지하고자할 때 사용 -->
+  <h1>{{num}}</h1>
+  <input type="text"
+  v-model.number="num" />
+
+<!-- v-model.trim : 양방향에 공백이 입력되면 공백을 지움 -->
+  <h1>{{msg}}</h1>
+  <input type="text"
+  v-model="msg" />
 </template>
 
 
@@ -23,8 +28,17 @@ export default {
     return {
       msg : 'Hello world!',
       checkbox : false,
-      kor : '안녕하세요.'
+      kor : '안녕하세요.',
+      num : 123
     } 
+  },
+  watch : {
+    num() {
+      console.log(typeof this.num)
+    },
+    msg() {
+      console.log(this.msg)
+    }
   }
 }
 </script>
