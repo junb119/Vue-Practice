4fe07cf6ea8ad5b188cf4a05d6a6aaeb53989c19
junb119 <emfowkd@gmail.com>
2022-09-18 14:37:23 +0900

    이벤트 핸들링 -keydown(키 수식어)

--- vue3-template-syntax/src/App.vue
+++ vue3-template-syntax/src/App.vue
@@ -1,44 +1,17 @@
 <template>
-  <div
-  class="parent"
-  @wheel.passive="handlerA">
-    <div
-    class="child">
-    </div>
-  </div>
-
-<!-- @wheel  : 휠을 움직일 때마다 핸들링
-passive : 화면과 로직을 따로 처리하여 화면을 최대한 부드럽게 전환하고 로직은 독립적으로 처리
--->
+  <input 
+    type="text"
+    @keydown.ctrl.shift.a="handler" />
+    <!-- keydown : 키를 눌렀을 때 이벤트 실행-->
 </template>
 
 <script>
 export default {
-  methods: {
-    handlerA (event) {
-      for (let i = 0; i<10000; i += 1) {
-        console.log(event)
-      }
+  methods : {
+    handler() {
+      console.log('Enter')
     }
   }
+  
 }
 </script>
-
-<style scoped lang ='scss'>
-  .parent {
-    width: 200px;
-    height: 100px;
-    background-color: royalblue;
-    margin: 10px;
-    padding: 10px;
-    overflow: auto;
-    .child {
-      width: 100px;
-      height: 2000px;
-      background-color: orange;
-
-      }
-    }
-
-</style>
-
